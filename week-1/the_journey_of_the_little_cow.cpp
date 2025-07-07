21b7125f5e8fbca66f71af5a19dcbc482bf93a92
chaoticJester <evoltogaming@gmail.com>
2025-07-07 14:33:32 +0700

    Add complete the journey of the little cow

--- week-1/the_journey_of_the_little_cow.cpp
+++ week-1/the_journey_of_the_little_cow.cpp
@@ -1,53 +1,56 @@
 #include <iostream>
 #include <cmath>
+#include <iomanip>
 using namespace std;
 
-int promaxMilk(int f, double P, double K){
-    double n = 2 * (f * P);
-    if(n == K) {
-        return 1;
-    }
-    return 0;
-}
-
-int normalMilk(int f, double P, double K){
-    double n = (f * P);
-    if(n >= K) {
-        return 1;
-    }
-    return 0;
-}
-
 int main() {
-    int j;
-    double K;
+    //รับ input j, K, d มา
+    int expo;
+    double require;
     int day;
-    
-    cin >> j;
-    cin >> K;
+    cin >> expo >> require;
     cin >> day;
-    
-    long double P = pow(10.0L, j);
-    
+
+    //คำนวนค่า P และกำหนดตัวแปรอื่นๆ
+    double P = 1 * pow(10, expo);
+    int food;
+    double total_nutrient = 0;
     int milk = 0;
     int promax_milk = 0;
-    double cow_food;
-    long double nutrients = 0;
-    
+
+    //EPSILON
+    const double EPS = 1e-9;
+
+    //รับ input จำนวนเม็ดอาหารในแต่ละวัน
     for(int i = 0; i < day; i++) {
-        cin >> cow_food;
-        nutrients += cow_food;
-        if(normalMilk(cow_food, P, K)) {
-            milk ++;
+        cin >> food;
+
+        //คำนวนสารอาหารแบบลุงและแบบโชค
+        double N_uncle = food * P; 
+
+        double N_chok = 0;
+        for(int j = 0; j < food; j++) {
+            N_chok += P;
         }
-        if(promaxMilk(cow_food, P, K)) {
-            promax_milk ++;
+
+        //เช็คว่าผลิตนมสูตรปกติได้ไหม
+        if( fabs(N_chok / require) > 1.00 || fabs(N_chok / require - 1.00) < EPS) {
+            // cout << "M : " << fabs(N_chok / require) << " " << N_chok / require << endl;
+            milk++;
+            // cout << "milk : " << milk << endl;
         }
+        //เช็คว่าผลิตนมสูตรโปรแมกส์ได้ไหม
+        if(fabs((N_chok + N_uncle) - require) < EPS) {
+            promax_milk++;
+        }
+        // printf("N_chok : %.17lf, N_uncle : %.17lf\n", N_chok, N_uncle);
+        total_nutrient += N_chok;
     }
-    
-    printf("Milk : %d\n", milk);
-    printf("ProMax Milk : %d\n", promax_milk);
-    printf("Nutrient : %17Lf", nutrients);
-    
+
+    //Output 
+    cout << "Milk : " << milk << endl;
+    cout << "ProMax Milk : " << promax_milk << endl;
+    cout << fixed << setprecision(17) << "Nutrient : " << total_nutrient << endl;
+
     return 0;
 }
