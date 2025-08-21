0376f74a81b8097d72df54023016c27576f96049
chaoticJester <evoltogaming@gmail.com>
2025-08-21 13:10:29 +0700

    Added week-5

---
+++ week-5/moo-ving_library_rescue.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <cctype>
+using namespace std;
+
+
+int countAscii(string bookName) {
+    int sumAscii = 0;
+    for(char c : bookName) {
+        sumAscii += (int)c;
+    }
+    return sumAscii;
+}
+
+void sortShelf(vector<string> &shelf) {
+    int n = shelf.size();
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n - i - 1; j++) {
+            if(shelf[j] > shelf[j + 1]) {
+                string tmpBook = shelf[j];
+                shelf[j] = shelf[j + 1];
+                shelf[j + 1] = tmpBook;
+            }
+        }
+    }
+}
+
+bool isValid(string name) {
+    if(name.empty()) {
+        return false;
+    }
+    if(!isalpha(name[0])) {
+        return false;
+    }
+    for(char c : name) {
+        int ascii = (int)c;
+        if(ascii < 32 || ascii > 126) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    int width;
+    cin >> width;
+    if(width < 256) {
+        map<char, vector<vector<string>>> shelves;
+        
+        string bookName;
+        bool isError = false;
+        while(cin >> bookName) {
+            if(bookName.length() > 30 || !isValid(bookName)) {
+                isError = true;
+                break;
+            }
+            char shelfName = bookName[0];
+            int hashIndex = countAscii(bookName) % width;
+
+            if(shelves[shelfName].empty()) {
+                shelves[shelfName] = vector<vector<string>>(width);
+            }
+            shelves[shelfName][hashIndex].push_back(bookName);
+        }
+
+        if(!isError) {
+            if(shelves.empty()) {
+                cout << "All shelfs are empty!\n";
+            } else {
+                for(auto &subShelf : shelves) {
+                    char shelfName = subShelf.first;
+                    auto &bookList = subShelf.second;
+
+                    printf("Shelf %c:\n", shelfName);
+
+                    for(int i = 0; i < bookList.size(); i++) {
+                        printf("%c%d: ", shelfName, i + 1);
+                        if(bookList[i].empty()) {
+                            printf("shelf is empty!\n");
+                            continue;
+                        }
+
+                        sortShelf(bookList[i]);
+                        for(int j = 0; j < bookList[i].size(); j++) {
+                            cout << bookList[i][j] << " ";
+                        }
+                        printf("\n");
+
+                    }
+                }
+            }
+        }
+    }
+    return 0;
+}
